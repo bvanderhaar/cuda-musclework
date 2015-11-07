ddc90146c01bc37f637e1cfbf31eba0453f323d1
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-06 21:22:19 -0500

    add error checking:

--- main.cu
+++ main.cu
@@ -7,6 +7,18 @@
  */
 #define BLOCK_SIZE 32
 
+#define gpuErrchk(ans)                                                         \
+  { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line,
+                      bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort)
+      exit(code);
+  }
+}
+
 __global__ void cu_dotProduct(long long *distance_array_d,
                               long long *force_array_d,
                               long long *result_array_d, long long max) {
@@ -45,11 +57,14 @@ extern "C" void gpu_dotProduct(long long *result_array, long long num_vectors) {
   long long *result_array_d;
 
   // allocate space in the device
-  cudaMalloc((void **)&distance_array_d, sizeof(long long) * num_vectors);
-  cudaMalloc((void **)&force_array_d, sizeof(long long) * num_vectors);
-  cudaMalloc((void **)&result_array_d, sizeof(long long) * num_vectors);
+  gpuErrchk(
+      cudaMalloc((void **)&distance_array_d, sizeof(long long) * num_vectors));
+  gpuErrchk(
+      cudaMalloc((void **)&force_array_d, sizeof(long long) * num_vectors));
+  gpuErrchk(
+      cudaMalloc((void **)&result_array_d, sizeof(long long) * num_vectors));
 
-  //cudaMemcpy(result_array_d, result_array, sizeof(long long) * num_vectors,
+  // cudaMemcpy(result_array_d, result_array, sizeof(long long) * num_vectors,
   //           cudaMemcpyHostToDevice);
 
   // set execution configuration
@@ -57,17 +72,24 @@ extern "C" void gpu_dotProduct(long long *result_array, long long num_vectors) {
   dim3 dimgrid(ceil((long double)num_vectors / BLOCK_SIZE));
 
   cu_gen_force_array<<<dimgrid, dimblock>>>(force_array_d, num_vectors);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
   cu_gen_distance_array<<<dimgrid, dimblock>>>(distance_array_d, num_vectors);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
   cu_dotProduct<<<dimgrid, dimblock>>>(distance_array_d, force_array_d,
                                        result_array_d, num_vectors);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
 
   cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess) {
     printf("CUDA errors: %s\n", cudaGetErrorString(err));
   }
   // transfer results back to host
-  cudaMemcpy(result_array, result_array_d, sizeof(long long) * num_vectors,
-             cudaMemcpyDeviceToHost);
+  gpuErrchk(cudaMemcpy(result_array, result_array_d,
+                       sizeof(long long) * num_vectors,
+                       cudaMemcpyDeviceToHost));
   // release the memory on the GPU
   cudaFree(distance_array_d);
   cudaFree(force_array_d);
